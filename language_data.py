63215f832973847c53d93de91780ffe83309a6a9
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-09-19 21:09:42 +0700

    update language_data.py

--- language_data.py
+++ language_data.py
@@ -12,12 +12,14 @@ from ..language_data import update_exc, strings_to_exc
 
 # import language-specific data from files
 #from .tag_map import TAG_MAP
+from .tag_map import TAG_MAP
 from .stop_words import STOP_WORDS
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 
 
-#TAG_MAP = dict(TAG_MAP)
+TAG_MAP = dict(TAG_MAP)
 STOP_WORDS = set(STOP_WORDS)
-
+TOKENIZER_EXCEPTIONS = dict(TOKENIZER_EXCEPTIONS)
 
 # export __all__ = ["TAG_MAP", "STOP_WORDS"]
-__all__ = ["STOP_WORDS"]
+__all__ = ["TAG_MAP", "STOP_WORDS","TOKENIZER_EXCEPTIONS"]
