6e2c6579f1add63941eb8e61ea4f9c3190dfecd8
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-09-19 21:42:46 +0700

    add ThaiTokenizer

--- __init__.py
+++ __init__.py
@@ -3,11 +3,29 @@ from __future__ import unicode_literals, print_function
 
 from os import path
 
-from ..language import Language
+from ..language import Language, BaseDefaults
 from ..attrs import LANG
-
+from ..tokenizer import Tokenizer
+from ..tokens import Doc
 from .language_data import *
+class ThaiTokenizer(object):
+    def __init__(self, cls, nlp=None):
+        self.vocab = nlp.vocab if nlp is not None else cls.create_vocab(nlp)
+        try:
+            from pythainlp.tokenize import word_tokenize
+        except ImportError:
+            raise ImportError("The Thai tokenizer requires the PyThaiNLP library: "
+                              "https://github.com/wannaphongcom/pythainlp/")
+        self.tokenizer = word_tokenize
 
+    def __call__(self, text):
+        words = [x.surface for x in self.tokenizer(text,engine='newmm')]
+        return Doc(self.vocab, words=words, spaces=[False]*len(words))
+
+class ThaiDefaults(BaseDefaults):
+	@classmethod
+	def create_tokenizer(cls, nlp=None):
+		return ThaiTokenizer(cls, nlp)
 
 class Thai(Language):
     lang = 'th'
@@ -19,3 +37,6 @@ class Thai(Language):
         tokenizer_exceptions = TOKENIZER_EXCEPTIONS
         tag_map = TAG_MAP
         stop_words = STOP_WORDS
+	def make_doc(self, text):
+		words = self.tokenizer(text,engine='newmm')
+		return Doc(self.vocab, words=words, spaces=[False]*len(words))
